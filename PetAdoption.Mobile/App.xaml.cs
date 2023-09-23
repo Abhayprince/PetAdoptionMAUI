6e699067870f18216ba43c324093b861cfdc39f4
Abhay Prince <46496449+Abhayprince@users.noreply.github.com>
2023-09-23 19:19:45 +0530

    Add project files.

---
+++ PetAdoption.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PetAdoption.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PetAdoption.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PetAdoption.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
