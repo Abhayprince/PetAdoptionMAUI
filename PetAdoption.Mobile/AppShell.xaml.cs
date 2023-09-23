128d5dadb913c89ac8cb712e0c3e15fd84f4cf26
Abhay Prince <46496449+Abhayprince@users.noreply.github.com>
2023-09-23 23:47:17 +0530

    Add Flyout header and footer

--- PetAdoption.Mobile/AppShell.xaml.cs
+++ PetAdoption.Mobile/AppShell.xaml.cs
@@ -6,5 +6,7 @@
         {
             InitializeComponent();
         }
+
+        private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e) => await Launcher.OpenAsync("https://www.youtube.com/@abhayprince");
     }
 }
